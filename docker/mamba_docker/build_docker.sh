24a6503cef0fbf56ce1d149fddd62e16baa45583
Alex-Romanko <romanko.aleksandr.a@gmail.com>
2024-02-28 16:04:37 +0300

    update docker immage name

--- docker/mamba_docker/build_docker.sh
+++ docker/mamba_docker/build_docker.sh
@@ -4,5 +4,5 @@ set -ev
 
 VERSION=`cat VERSION.txt`
 
-docker buildx build -t alex-romanko/mamba_rnaseq:${VERSION} .
-docker buildx build -t alex-romanko/mamba_rnaseq:latest .
+docker buildx build -t alexromanko/mamba_rnaseq:${VERSION} .
+docker buildx build -t alexromanko/mamba_rnaseq:latest .
